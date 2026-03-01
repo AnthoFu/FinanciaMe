cec05fa5a581d1b89acaa43801ec550feef7de63
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-03-01 13:00:03 -0400

    [V1.11.5] fix: Corregido error visual en el modo claro

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -6,10 +6,10 @@ import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useAppTheme } from '@/context/ThemeContext';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const { colorScheme } = useAppTheme();
 
   return (
     <Tabs
